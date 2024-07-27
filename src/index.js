3a87c42aa4b6f4db4e2de7df4033bfd980c702ee
rjm <robjmass@gmail.com>
2024-07-27 15:21:25 -0400

    working page without react or ts

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Hello, React with Electron!</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
