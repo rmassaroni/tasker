6a144f3b3e51e05112bd15bcb832a7757d95643b
rjm <robjmass@gmail.com>
2024-07-29 17:22:36 -0400

    some fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,44 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
+    // main: 'src/main.jsx',
+    // root: path.resolve(__dirname),
+    // root: 'public',
     root: 'src',
     build: {
         outDir: '../build',
+        // outDir: '../dist',
+        // emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: '/src/main.jsx'
+                main: '/src/main.jsx',
             }
+            // input: path.resolve(__dirname, 'src/main.jsx'),
         }
-    }
+    },
+    // publicDir: '../public',
+    // publicDir: '/public',
+    // server: {
+        // port: 5173,
+        // open: true
+    // },
+    // plugins: [react()],
+    // root: path.resolve(__dirname),
+    // // root: 'public',
+    // build: {
+    //     outDir: path.resolve(__dirname, 'dist'), 
+    //     emptyOutDir: true,
+    //     rollupOptions: {
+    //         input: path.resolve(__dirname, 'src/main.jsx'), 
+    //     },
+    // },
+    // publicDir: 'public',
+    // server: {
+    //     port: 5173,
+    //     // open: true, 
+    // },
 });
 
